2e0cca095b236e850e62bd440222ab6bf400c6b1
Ruben-Ascencio <ascencioperezr@gmail.com>
2022-11-21 12:09:45 -0600

    Escogemos archivo y enviamos solo el nombre.

--- PRACTICAS/PRACTICA2/Practica2/src/BroadcastEnvia.java
+++ PRACTICAS/PRACTICA2/Practica2/src/BroadcastEnvia.java
@@ -1,20 +1,32 @@
+import java.io.File;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 public class BroadcastEnvia{
     public static void main(String[] args) throws Exception{
         final InetAddress objIA = InetAddress.getByName("224.0.0.1");
         final DatagramSocket objDS = new DatagramSocket();
-        Scanner teclado = new Scanner(System.in);
+        FileNameExtensionFilter objFNEF = new FileNameExtensionFilter(".bmp, .png, .jpg, .txt", "bmp", "png", "jpg", "txt");
+        JFileChooser objJFC = new JFileChooser();
         while(true){
-            String enviar = teclado.nextLine();
-            byte[] buffer = enviar.getBytes();
-            DatagramPacket objDP = new DatagramPacket(buffer, buffer.length, objIA, 9797);
-            objDS.send(objDP);
-            if(enviar.equalsIgnoreCase("SALIR")){break;}
+            objJFC.setFileFilter(objFNEF);
+            int opcion = objJFC.showOpenDialog(null);
+            if(opcion == JFileChooser.APPROVE_OPTION){
+                File objF = objJFC.getSelectedFile();
+                byte[] buffer = objF.getName().getBytes();
+                DatagramPacket objDP = new DatagramPacket(buffer, buffer.length, objIA, 9797);
+                objDS.send(objDP);
+            }else{
+                String salir = "SALIR";
+                byte[] buffer = salir.getBytes();
+                DatagramPacket objDP = new DatagramPacket(buffer, buffer.length, objIA, 9797);
+                objDS.send(objDP);
+                break;
+            }
         }
-        teclado.close();
         objDS.close();
     }
 }
