d6c487f6a527efc4eba62d7b057551c754bd88bb
Ruben-Ascencio <ascencioperezr@gmail.com>
2022-11-21 16:39:23 -0600

    Enviamos paquetes completos, falta crear el archivo y guardar la carpeta

--- PRACTICAS/PRACTICA2/Practica2/src/BroadcastEnvia.java
+++ PRACTICAS/PRACTICA2/Practica2/src/BroadcastEnvia.java
@@ -1,10 +1,7 @@
-import java.io.File;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.*;
+import java.net.*;
 public class BroadcastEnvia{
     public static void main(String[] args) throws Exception{
         final InetAddress objIA = InetAddress.getByName("224.0.0.1");
@@ -16,9 +13,18 @@ public class BroadcastEnvia{
             int opcion = objJFC.showOpenDialog(null);
             if(opcion == JFileChooser.APPROVE_OPTION){
                 File objF = objJFC.getSelectedFile();
-                byte[] buffer = objF.getName().getBytes();
-                DatagramPacket objDP = new DatagramPacket(buffer, buffer.length, objIA, 9797);
-                objDS.send(objDP);
+                FileInputStream objFIS = new FileInputStream(objF);
+                BufferedInputStream objbis = new BufferedInputStream(objFIS);
+                byte buffer[] = new byte[200];
+                int aux = 0;
+                String lineas = null;
+                while((aux = objbis.read(buffer)) != -1){
+                    lineas = new String(buffer, 0, aux);
+                    DatagramPacket objDP = new DatagramPacket(lineas.getBytes(), lineas.length(), objIA, 9797);
+                    objDS.send(objDP);
+                    System.out.println(lineas);
+                }
+                objFIS.close();
             }else{
                 String salir = "SALIR";
                 byte[] buffer = salir.getBytes();
--- PRACTICAS/PRACTICA2/Practica2/src/BroadcastRecibe.java
+++ PRACTICAS/PRACTICA2/Practica2/src/BroadcastRecibe.java
@@ -1,10 +1,18 @@
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
+import java.io.*;
+import java.net.*;
+import java.time.*;
 public class BroadcastRecibe{
     public static void main(String[] args)throws Exception{
         final InetAddress objIA = InetAddress.getByName("224.0.0.1");
         final MulticastSocket objMS = new MulticastSocket(9797);
+        LocalDateTime objLDT = LocalDateTime.now();
+        String ruta = "Documentos/" + objLDT.getHour() + "_" + objLDT.getMinute() + "_" + objLDT.getSecond();
+        File objCarpeta = new File(ruta);
+        if(!objCarpeta.exists()){
+            if(objCarpeta.mkdirs()){System.out.println("Carpeta creada\n" + ruta);}
+            else{System.out.println("No se creo la carpeta");}
+        }
+        else{System.out.println("La carpeta ya existe");}
         objMS.joinGroup(objIA);
         while(true){
             byte[] buffer = new byte[200];
