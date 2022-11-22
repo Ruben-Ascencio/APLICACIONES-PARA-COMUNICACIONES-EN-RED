66bc2b473ceab2ba40b96438b66bf4377375ea9b
Ruben-Ascencio <ascencioperezr@gmail.com>
2022-11-21 21:11:30 -0600

    Multicast. Enviamos archivos planos. -> A github

--- PRACTICAS/PRACTICA2/Practica2/src/BroadcastEnvia.java
+++ PRACTICAS/PRACTICA2/Practica2/src/BroadcastEnvia.java
@@ -1,38 +1,31 @@
 import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.*;
 import java.net.*;
 public class BroadcastEnvia{
     public static void main(String[] args) throws Exception{
         final InetAddress objIA = InetAddress.getByName("224.0.0.1");
         final DatagramSocket objDS = new DatagramSocket();
-        FileNameExtensionFilter objFNEF = new FileNameExtensionFilter(".bmp, .png, .jpg, .txt", "bmp", "png", "jpg", "txt");
         JFileChooser objJFC = new JFileChooser();
-        while(true){
-            objJFC.setFileFilter(objFNEF);
-            int opcion = objJFC.showOpenDialog(null);
-            if(opcion == JFileChooser.APPROVE_OPTION){
-                File objF = objJFC.getSelectedFile();
-                FileInputStream objFIS = new FileInputStream(objF);
-                BufferedInputStream objbis = new BufferedInputStream(objFIS);
-                byte buffer[] = new byte[200];
-                int aux = 0;
-                String lineas = null;
-                while((aux = objbis.read(buffer)) != -1){
-                    lineas = new String(buffer, 0, aux);
-                    DatagramPacket objDP = new DatagramPacket(lineas.getBytes(), lineas.length(), objIA, 9797);
-                    objDS.send(objDP);
-                    System.out.println(lineas);
-                }
-                objFIS.close();
-            }else{
-                String salir = "SALIR";
-                byte[] buffer = salir.getBytes();
-                DatagramPacket objDP = new DatagramPacket(buffer, buffer.length, objIA, 9797);
+        int opcion = objJFC.showOpenDialog(null);
+        if(opcion == JFileChooser.APPROVE_OPTION){
+            File objF = objJFC.getSelectedFile();
+            FileInputStream objFIS = new FileInputStream(objF);
+            BufferedInputStream objbis = new BufferedInputStream(objFIS);
+            byte buffer[] = new byte[1024];
+            int aux = 0;
+            String lineas = null;
+            while((aux = objbis.read(buffer)) != -1){
+                lineas = new String(buffer, 0, aux);
+                DatagramPacket objDP = new DatagramPacket(lineas.getBytes(), lineas.length(), objIA, 9797);
                 objDS.send(objDP);
-                break;
             }
+            System.out.println("Archivo enviado");
+            objFIS.close();
         }
+        String salir = "SALIR";
+        byte[] buffer = salir.getBytes();
+        DatagramPacket objDP = new DatagramPacket(buffer, buffer.length, objIA, 9797);
+        objDS.send(objDP);
         objDS.close();
     }
 }
--- PRACTICAS/PRACTICA2/Practica2/src/BroadcastRecibe.java
+++ PRACTICAS/PRACTICA2/Practica2/src/BroadcastRecibe.java
@@ -9,19 +9,22 @@ public class BroadcastRecibe{
         String ruta = "Documentos/" + objLDT.getHour() + "_" + objLDT.getMinute() + "_" + objLDT.getSecond();
         File objCarpeta = new File(ruta);
         if(!objCarpeta.exists()){
-            if(objCarpeta.mkdirs()){System.out.println("Carpeta creada\n" + ruta);}
+            if(objCarpeta.mkdirs()){System.out.println("Carpeta creada: " + ruta);}
             else{System.out.println("No se creo la carpeta");}
         }
         else{System.out.println("La carpeta ya existe");}
         objMS.joinGroup(objIA);
+        FileOutputStream objFOS = new FileOutputStream(ruta + "/" + "Archivo");
         while(true){
-            byte[] buffer = new byte[200];
+            byte[] buffer = new byte[1024];
             DatagramPacket objDP = new DatagramPacket(buffer, buffer.length);
             objMS.receive(objDP);
             String paquete = new String(objDP.getData()).trim();
-            System.out.println(String.format("%s", paquete));
             if(paquete.equalsIgnoreCase("SALIR")){break;}
+            else{objFOS.write(paquete.getBytes());}
         }
+        System.out.println("Archivo creado en su carpeta");
+        objFOS.close();
         objMS.leaveGroup(objIA);
         objMS.close();
     }
