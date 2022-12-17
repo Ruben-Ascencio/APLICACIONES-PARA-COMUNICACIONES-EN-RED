552a24acccf22efca393545e40b2c08a3ffd11ce
Ruben-Ascencio <ascencioperezr@gmail.com>
2022-12-17 03:23:15 -0600

    Envio de archivos TCP terminado

--- PRACTICAS/PRACTICA2/Cliente.java
+++ PRACTICAS/PRACTICA2/Cliente.java
@@ -1,46 +1,47 @@
 import java.io.*;
 import java.net.*;
-import javax.swing.JFileChooser;
+import javax.swing.*;
 public class Cliente{
     public static void main(String[] args) throws Exception{
         DataOutputStream objOOS = null;
         Socket objS = null;
         try{
-            objS = new Socket("127.0.0.1", 8080);
-            objOOS = new DataOutputStream(objS.getOutputStream());
-            JFileChooser objJFC = new JFileChooser();
+            objS = new Socket("127.0.0.1", 8080);                       //Creamos nuestro Socket.
+            objOOS = new DataOutputStream(objS.getOutputStream());      //Creamos nuestro objeto objOOS, que nos permite capturar todo lo que llegue a nuestro Socket
+            JFileChooser objJFC = new JFileChooser();                   //Creamos este objeto para poder abrir una ventana para seleccionar nuestro archivo
             File archivoSeleccionado;
             while(true){
-                int res = objJFC.showOpenDialog(null);
+                int res = objJFC.showOpenDialog(null);                  //Capturamos la opcion que seleccione el usuario, si da clic en Aceptar, entonces subimos nuestro archivo
                 if(res == JFileChooser.APPROVE_OPTION){
-                    archivoSeleccionado = objJFC.getSelectedFile();
-                    String nombre = archivoSeleccionado.getName();
-                    String ruta = archivoSeleccionado.getAbsolutePath();
-                    long tam = archivoSeleccionado.length();
-                    DataInputStream objDIS = new DataInputStream(new FileInputStream(ruta));
-                    objOOS.writeUTF(nombre);
-                    objOOS.flush();
-                    objOOS.writeLong(tam);
-                    objOOS.flush();
+                    objOOS.writeInt(0);                                 //Enviamos el valor 0 para decirle al servidor que subiremos un archivo
+                    archivoSeleccionado = objJFC.getSelectedFile();     //Obtenemos el archivo seleccionado.
+                    String nombre = archivoSeleccionado.getName();      //Obtenemos el nombre del archivo
+                    String ruta = archivoSeleccionado.getAbsolutePath();    //Obtenemos la ruta absoluta del archivo
+                    long tam = archivoSeleccionado.length();                //Obtenemos el tamano total (en bytes) del archivo
+                    DataInputStream objDIS = new DataInputStream(new FileInputStream(ruta));    //Creamos un objeto de tipo DataInputStream, que nos permitira abrir el archivo seleccionado en modo binario (con la clase FileInput)
+                    objOOS.writeUTF(nombre);                            //Le enviamos al servidor el nombre del archivo
+                    objOOS.flush();                                     //Limpiamos el canal/buffer
+                    objOOS.writeLong(tam);                              //Enviamos el tamano total del archivo al servidor
+                    objOOS.flush();                                     //Limpiamos el canal/buffer
                     long enviado = 0;
                     int a = 0, porcentaje = 0;
-                    byte[] paquete = new byte[200];
-                    while(enviado < tam){
-                        a = objDIS.read(paquete);
-                        objOOS.write(paquete, 0, a);
-                        objOOS.flush();
-                        enviado += a;
-                        porcentaje = (int)((enviado * 100) / tam);
-                        System.out.println("Enviado el "+porcentaje+"%");
+                    byte[] paquete = new byte[200];                     //Creamos nuestro paquete de bytes, con un tamano de 200 bytes
+                    while(enviado < tam){                               //Realizamos un ciclo hasta que alcancemos el tamano total del archivo
+                        a = objDIS.read(paquete);                       //Capturamos el total de bytes leidos del archivo y capturados en nuestro paquete
+                        objOOS.write(paquete, 0, a);                    //Enviamos los bloques de bytes de nuestro archivo al servidor
+                        objOOS.flush();                                 //Limpiamos el canal/buffer
+                        enviado += a;                                   //Incrementamos nuestra variable que nos indica la cantidad de bytes enviados
+                        porcentaje = (int)((enviado * 100) / tam);      //Operacion para sacar el porcentaje enviado
+                        System.out.println("Enviado el "+porcentaje+"%");   //Imprimimos la operacion
                     }
-                    System.out.println("Se ha subido el archivo");
-                    objDIS.close();
-                }else{break;}
+                    System.out.println("Se ha subido el archivo");      //Mostramos mensaje
+                    objDIS.close();                                     //Cerramos el archivo
+                }else{objOOS.writeInt(1); break;}                       //Si el cliente ya no desea subir el arhcivo, entonces le enviamos al servidor el codigo 1 y rompemos el ciclo
             }
-        }catch(Exception e){e.printStackTrace();}
+        }catch(Exception e){e.printStackTrace();}                       //Capturamos errores
         finally{
-            if(objOOS != null){objOOS.close();}
-            if(objS != null){objS.close();}
+            if(objOOS != null){objOOS.close();}                         //Cerramos el objeto que nos permitia enviar nuestros datos
+            if(objS != null){objS.close();}                             //Cerramos el Socket
         }
     }
 }
--- PRACTICAS/PRACTICA2/Servidor.java
+++ PRACTICAS/PRACTICA2/Servidor.java
@@ -1,46 +1,59 @@
 import java.io.*;
 import java.net.*;
+import java.time.*;
+import java.time.LocalDateTime;
 public class Servidor{
     public static void main(String[] args) throws Exception{
-        Socket objS = null;
-        ServerSocket objSS = new ServerSocket(8080);
-        while(true){
+        Socket objS = null;                                         //Iniciamos nuestro Socket en null
+        ServerSocket objSS = new ServerSocket(8080);                //Objeto cuya funcion es equivalente a la funcion accept en C
+        while(true){                                                //Ciclo infinito
             try{
-                objS = objSS.accept();
-                (new Ejecucion(objS)).start();
-            }catch(Exception e){e.printStackTrace();}
+                objS = objSS.accept();                              //Aceptamos la conexion entrante
+                (new Ejecucion(objS)).start();                      //Arrancamos nuestro metodo Ejecucion con un hilo
+            }catch(Exception e){e.printStackTrace();}               //Capturamos errores
         }
     }
     static class Ejecucion extends Thread{
         private Socket objS = null;
         private DataInputStream objOIS = null;;
-        public Ejecucion(Socket objS){this.objS = objS;}
+        public Ejecucion(Socket objS){this.objS = objS;}            //Constructor de la clase
+        @Override
         public void run(){
             try{
-                objOIS = new DataInputStream(objS.getInputStream());
-                String nombre = objOIS.readUTF();
-                long tam = objOIS.readLong();
-                String direccion = "DOCUMENTOS/" + nombre;
-                DataOutputStream objDOS = new DataOutputStream(new FileOutputStream(direccion));
-                long recibido = 0;
-                int a = 0, porciento = 0;
-                byte[] paquete =  new byte[200];
-                while(recibido < tam){
-                    a = objOIS.read(paquete);
-                    objDOS.write(paquete, 0, a);
-                    objDOS.flush();
-                    recibido += a;
-                    porciento = (int)((recibido * 100) / tam);
-                    System.out.println("Se ha recibido el " +porciento+ "%");
+                objOIS = new DataInputStream(objS.getInputStream());//Creamos un objeto de tipo DataInputStream para capturar los datos de entrada de nuestro socket
+                LocalDateTime objLDT = LocalDateTime.now();         //Creamos un objeto de tipo LocalDateTime
+                /* Bloque donde creamos las carpetas para cada usuario, el nombre de nuestras carpetas concatena la hora, minuto, segundo y numero del dia*/
+                String direccion = "DOCUMENTOS/" + objLDT.getHour() + "_" + objLDT.getMinute() + "_" + objLDT.getSecond() + "_" + objLDT.getDayOfYear();
+                File directorio = new File(direccion);
+                if(!directorio.exists()){
+                    if(directorio.mkdirs()){System.out.println("Carpeta creada correctamente");}
+                    else{System.out.println("No se creo la carpeta"); System.exit(0);}
+                }else{System.out.println("La carpeta ya existe");System.exit(0);}
+                while(true){
+                    int llave = objOIS.readInt();                   //Capturamos la llave enviada del cliente, si es 1, rompemos el ciclo. De lo contrario, el cliente subira un archivo
+                    if(llave == 0){
+                        String nombre = objOIS.readUTF();           //Obtenemos el nombre del archivo
+                        long tam = objOIS.readLong();               //Obtenemos el tamano del archivo
+                        DataOutputStream objDOS = new DataOutputStream(new FileOutputStream(direccion + "/" + nombre));     //Creamos el archivo y lo escribiremos en su respectiva carpeta
+                        long recibido = 0;
+                        int a = 0;
+                        byte[] paquete =  new byte[200];            //Creamos nuestro paquete de bytes que recibira todos los bytes de los archivos seleccionados
+                        while(recibido < tam){                      //Realizamos un ciclo hasta que alcancemos el tamano total del archivo
+                            a = objOIS.read(paquete);               //Capturamos el total de bytes recibidos del archivo y capturados en nuestro paquete
+                            objDOS.write(paquete, 0, a);            //Escribimmos nuestro archivo
+                            objDOS.flush();                         //Limpiamos el buffer
+                            recibido += a;                          //Incrementamos nuestra variable que nos indica la cantidad de bytes recibidos
+                        }
+                        System.out.println(nombre);                 //Imprimimos el nombre del archivo
+                        objDOS.close();                             //Cerramos nuestro archivo
+                    }else{break;}                                   //Rompemos la ejecucion del hilo si el cliente ya no desea subir un archivo
                 }
-                System.out.println(nombre);
-                objDOS.close();
-            }catch(Exception e){e.printStackTrace();}
+            }catch(Exception e){e.printStackTrace();}               //Capturamos errores
             finally{
                 try{
-                    if(objOIS != null){objOIS.close();}
-                    if(objS != null){objS.close();}
-                }catch(Exception ex){ex.printStackTrace();}
+                    if(objOIS != null){objOIS.close();}             //Cerramos el objeto que nos permite recibir lo que mande el cliente
+                    if(objS != null){objS.close();}                 //Cerramos el canal
+                }catch(Exception ex){ex.printStackTrace();}         //Capturamo errores
             }
         }
     }
